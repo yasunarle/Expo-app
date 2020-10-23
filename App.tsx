ffea121b8a93edfedcc8007e79e89d94e770d5d9
Yasunari t <u7df25c588m737@gmail.com>
2020-10-23 14:11:36 +0900

    ⛹️‍♀️practice: react-spring

--- App.tsx
+++ App.tsx
@@ -1,12 +1,25 @@
 import { StatusBar } from 'expo-status-bar'
-import React from 'react'
-import { StyleSheet, Text, View } from 'react-native'
-// Plugins
-import './src/plugins/Firebase'
+import React, { useState } from 'react'
+import { StyleSheet, Text, View, ViewProps } from 'react-native'
+// react-spring
+import { useSpring, animated, useTransition } from 'react-spring/native'
+
+const AnimatedView = animated(View)
+const AnimatedText = animated(Text)
 
 export default function App() {
+  const springProps = useSpring({
+    opacity: 1,
+    from: { opacity: 0 },
+    to: { opacity: 1 },
+    delay: 200,
+  })
+
   return (
     <View style={styles.container}>
+      <AnimatedView style={springProps}>
+        <Text style={{ fontSize: 40 }}>spring</Text>
+      </AnimatedView>
       <Text>Open up App.tsx to start working on your app!</Text>
       <StatusBar style="auto" />
     </View>
---
+++ src/components/SampleSpring.tsx
@@ -0,0 +1,17 @@
+import React from 'react'
+import { View, Text, TouchableOpacity, StyleSheet } from 'react-native'
+import { animated } from 'react-spring/renderprops'
+
+const AnimatedView = animated(View)
+
+// type Props = {
+//   children: React.ReactNode
+// }
+const SampleSpringComponent = () => {
+  // return (
+  //   <AnimatedView>
+  //     <Text>test</Text>
+  //   </AnimatedView>
+  // )
+}
+export default SampleSpringComponent
